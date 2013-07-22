bb65a94752709119e70cdd6cd6a1ac8eaf32a191
Sven Lito <me@svenlito.com>
2013-07-22 16:22:35 +0100

    [wip] moat tests

--- test/specs/hoodie.spec.js
+++ test/specs/hoodie.spec.js
@@ -50,82 +50,92 @@ describe("Hoodie", function() {
       expect(Hoodie.prototype.checkConnection.called).to.be.ok();
     });
 
-    xit("store has to be initialized before remote", function() {
-      var hoodie, order;
+    //xit("store has to be initialized before remote", function() {
+      //var hoodie, order;
 
-      order = [];
+      //order = [];
 
-      this.sandbox.stub(Hoodie, 'LocalStore').returns(function() {
-        order.push('store');
-        new Mocks.Hoodie().store;
-      });
+      //this.sandbox.stub(Hoodie, 'LocalStore').returns(function() {
+        //order.push('store');
+        //new Mocks.Hoodie().store;
+      //});
 
-      this.sandbox.stub(Hoodie, 'AccountRemote').returns(function() {
-        order.push('remote');
-        new Mocks.Hoodie().remote;
-      });
+      //this.sandbox.stub(Hoodie, 'AccountRemote').returns(function() {
+        //order.push('remote');
+        //new Mocks.Hoodie().remote;
+      //});
 
-      hoodie = new window.Hoodie();
-      expect(order.join(',')).to.eql('store,remote');
-    });
+      //hoodie = new window.Hoodie();
+      //expect(order.join(',')).to.eql('store,remote');
+    //});
+
   });
 
-  //describe("#request(type, path, options)", function() {
+  describe("#request(type, path, options)", function() {
 
-    //// see http://bugs.jquery.com/ticket/14104
-    //it("should return a jQuery.ajax compatible promise", function() {
-      //var promise = this.hoodie.request('GET', '/');
-      //expect(promise).toBePromise();
-      //expect(promise.abort).toBeDefined();
-    //});
-    //_when("request('GET', '/')", function() {
-      //beforeEach(function() {
-        //var args;
-        //this.hoodie.request('GET', '/');
-        //this.args = args = $.ajax.mostRecentCall.args[0];
-      //});
-      //it("should send a GET request to http://couch.example.com/", function() {
-        //expect(this.args.type).toBe('GET');
-        //expect(this.args.url).toBe('http://couch.example.com/');
-      //});
-      //it("should set `dataType: 'json'", function() {
-        //expect(this.args.dataType).toBe('json');
-      //});
-      //it("should set `xhrFields` to `withCredentials: true`", function() {
-        //expect(this.args.xhrFields.withCredentials).toBe(true);
-      //});
-      //it("should set `crossDomain: true`", function() {
-        //expect(this.args.crossDomain).toBe(true);
-      //});
-    //});
+    // see http://bugs.jquery.com/ticket/14104
+    it("should return a jQuery.ajax compatible promise", function() {
+      var promise = this.hoodie.request('GET', '/');
+      expect(promise.abort).to.be.ok();
+    });
 
-    //_when("request 'POST', '/test', data: funky: 'fresh'", function() {
-      //beforeEach(function() {
-        //var args;
-        //this.hoodie.request('POST', '/test', {
-          //data: {
-            //funky: 'fresh'
-          //}
-        //});
-        //this.args = args = $.ajax.mostRecentCall.args[0];
-      //});
-      //it("should send a POST request to http://couch.example.com/test", function() {
-        //expect(this.args.type).toBe('POST');
-        //expect(this.args.url).toBe('http://couch.example.com/test');
-      //});
-    //});
+    _when("request('GET', '/')", function() {
 
-    //_when("request('GET', 'http://api.otherapp.com/')", function() {
-      //beforeEach(function() {
-        //var args;
-        //this.hoodie.request('GET', 'http://api.otherapp.com/');
-        //this.args = args = $.ajax.mostRecentCall.args[0];
-      //});
-      //it("should send a GET request to http://api.otherapp.com/", function() {
-        //expect(this.args.type).toBe('GET');
-        //expect(this.args.url).toBe('http://api.otherapp.com/');
-      //});
-    //});
+      beforeEach(function() {
+        var args;
+        this.hoodie.request('GET', '/');
+        this.args = args = $.ajax.args[0][0];
+      });
+
+      it("should send a GET request to http://couch.example.com/", function() {
+        expect(this.args.type).to.eql('GET');
+        expect(this.args.url).to.eql('http://couch.example.com/');
+      });
+      it("should set `dataType: 'json'", function() {
+        expect(this.args.dataType).to.eql('json');
+      });
+      it("should set `xhrFields` to `withCredentials: true`", function() {
+        expect(this.args.xhrFields.withCredentials).to.eql(true);
+      });
+      it("should set `crossDomain: true`", function() {
+        expect(this.args.crossDomain).to.eql(true);
+      });
+    });
+
+    _when("request 'POST', '/test', data: funky: 'fresh'", function() {
+
+      beforeEach(function() {
+        var args;
+        this.hoodie.request('POST', '/test', {
+          data: {
+            funky: 'fresh'
+          }
+        });
+        this.args = args = $.ajax.args[0][0];
+      });
+
+      it("should send a POST request to http://couch.example.com/test", function() {
+        expect(this.args.type).to.eql('POST');
+        expect(this.args.url).to.eql('http://couch.example.com/test');
+      });
+
+    });
+
+    _when("request('GET', 'http://api.otherapp.com/')", function() {
+
+      beforeEach(function() {
+        var args;
+        this.hoodie.request('GET', 'http://api.otherapp.com/');
+        this.args = args = $.ajax.args[0][0];
+      });
+
+      it("should send a GET request to http://api.otherapp.com/", function() {
+        expect(this.args.type).to.eql('GET');
+        expect(this.args.url).to.eql('http://api.otherapp.com/');
+      });
+
+    });
+
     //_when("request fails with empty response", function() {
       //beforeEach(function() {
         //this.ajaxDefer.reject({
@@ -141,137 +151,177 @@ describe("Hoodie", function() {
       //});
     //});
 
-  //});
+  });
 
-  //describe("#checkConnection()", function() {
-    //beforeEach(function() {
-      //this.requestDefer = this.hoodie.defer();
-      //this.hoodie._checkConnectionRequest = null;
-      //spyOn(this.hoodie, "request").andReturn(this.requestDefer.promise());
-      //spyOn(this.hoodie, "trigger");
-      //window.setTimeout.andReturn(null);
-    //});
-    //it("should send GET / request", function() {
-      //this.hoodie.checkConnection();
-      //expect(this.hoodie.request).wasCalledWith('GET', '/');
-    //});
-    //it("should only send one request at a time", function() {
-      //this.hoodie.checkConnection();
-      //this.hoodie.checkConnection();
-      //expect(this.hoodie.request.callCount).toBe(1);
-    //});
-    //_when("hoodie is online", function() {
-      //beforeEach(function() {
-        //this.hoodie.online = true;
-      //});
-      //_and("request succeeds", function() {
-        //beforeEach(function() {
-          //this.requestDefer.resolve({
-            //"couchdb": "Welcome",
-            //"version": "1.2.1"
-          //});
-          //this.hoodie.checkConnection();
-        //});
-        //it("should check again in 30 seconds", function() {
-          //expect(window.setTimeout).wasCalledWith(this.hoodie.checkConnection, 30000);
-        //});
-        //it("should not trigger `reconnected` event", function() {
-          //expect(this.hoodie.trigger).wasNotCalledWith('reconnected');
-        //});
-      //});
-      //_and("request fails", function() {
-        //beforeEach(function() {
-          //this.requestDefer.reject({
-            //"status": 0,
-            //"statusText": "Error"
-          //});
-          //this.hoodie.checkConnection();
-        //});
-        //it("should check again in 3 seconds", function() {
-          //expect(window.setTimeout).wasCalledWith(this.hoodie.checkConnection, 3000);
-        //});
-        //it("should trigger `disconnected` event", function() {
-          //expect(this.hoodie.trigger).wasCalledWith('disconnected');
-        //});
-      //});
-    //});
-    //_when("hoodie is offline", function() {
-      //beforeEach(function() {
-        //this.hoodie.online = false;
-      //});
-      //_and("request succeeds", function() {
-        //beforeEach(function() {
-          //this.requestDefer.resolve({
-            //"couchdb": "Welcome",
-            //"version": "1.2.1"
-          //});
-          //this.hoodie.checkConnection();
-        //});
-        //it("should check again in 30 seconds", function() {
-          //expect(window.setTimeout).wasCalledWith(this.hoodie.checkConnection, 30000);
-        //});
-        //it("should trigger `reconnected` event", function() {
-          //expect(this.hoodie.trigger).wasCalledWith('reconnected');
-        //});
-      //});
-      //_and("request fails", function() {
-        //beforeEach(function() {
-          //this.requestDefer.reject({
-            //"status": 0,
-            //"statusText": "Error"
-          //});
-          //this.hoodie.checkConnection();
-        //});
-        //it("should check again in 3 seconds", function() {
-          //expect(window.setTimeout).wasCalledWith(this.hoodie.checkConnection, 3000);
-        //});
-        //it("should not trigger `disconnected` event", function() {
-          //expect(this.hoodie.trigger).wasNotCalledWith('disconnected');
-        //});
-      //});
-    //});
-  //});
+  describe("#checkConnection()", function() {
 
-  //describe("#open(store, options)", function() {
-    //it("should instantiate a Remote instance", function() {
-      //spyOn(Hoodie, "Remote");
-      //this.hoodie.open("store_name", {
-        //option: "value"
-      //});
-      //expect(Hoodie.Remote).wasCalledWith(this.hoodie, {
-        //name: "store_name",
-        //option: "value"
-      //});
-    //});
-  //});
+    beforeEach(function() {
+      this.requestDefer = this.hoodie.defer();
+      this.hoodie._checkConnectionRequest = null;
 
-  //describe("#uuid(num = 7)", function() {
+      this.sandbox.stub(this.hoodie, 'request').returns(this.requestDefer.promise());
+      this.sandbox.stub(this.hoodie, 'trigger');
+      window.setTimeout.returns(null);
+    });
 
-    //it("should default to a length of 7", function() {
-      //expect(this.hoodie.uuid().length).toBe(7);
-    //});
+    it("should send GET / request", function() {
+      this.hoodie.checkConnection();
+      expect(this.hoodie.request.calledWith('GET', '/')).to.be.ok();
+    });
 
-    //_when("called with num = 5", function() {
-      //it("should generate an id with length = 5", function() {
-        //expect(this.hoodie.uuid(5).length).toBe(5);
-      //});
-    //});
-  //});
+    it("should only send one request at a time", function() {
+      this.hoodie.checkConnection();
+      this.hoodie.checkConnection();
+      expect(this.hoodie.request.callCount).to.eql(1);
+    });
+
+    _when("hoodie is online", function() {
+
+      beforeEach(function() {
+        this.hoodie.online = true;
+      });
+
+      _and("request succeeds", function() {
+
+        beforeEach(function() {
+          this.requestDefer.resolve({
+            "couchdb": "Welcome",
+            "version": "1.2.1"
+          });
+          this.hoodie.checkConnection();
+        });
+
+        it("should check again in 30 seconds", function() {
+          expect(window.setTimeout.calledWith(this.hoodie.checkConnection, 30000)).to.be.ok();
+        });
+
+        it("should not trigger `reconnected` event", function() {
+          expect(this.hoodie.trigger.calledWith('reconnected')).to.not.be.ok();
+        });
+
+      });
+
+      _and("request fails", function() {
+
+        beforeEach(function() {
+          this.requestDefer.reject({
+            "status": 0,
+            "statusText": "Error"
+          });
+          this.hoodie.checkConnection();
+        });
+
+        it("should check again in 3 seconds", function() {
+          expect(window.setTimeout.calledWith(this.hoodie.checkConnection, 3000)).to.be.ok();
+        });
+
+        it("should trigger `disconnected` event", function() {
+          expect(this.hoodie.trigger.calledWith('disconnected')).to.be.ok();
+        });
+
+      });
+
+    });
+
+    _when("hoodie is offline", function() {
+
+      beforeEach(function() {
+        this.hoodie.online = false;
+      });
+
+      _and("request succeeds", function() {
+
+        beforeEach(function() {
+          this.requestDefer.resolve({
+            "couchdb": "Welcome",
+            "version": "1.2.1"
+          });
+          this.hoodie.checkConnection();
+        });
+
+        it("should check again in 30 seconds", function() {
+          expect(window.setTimeout.calledWith(this.hoodie.checkConnection, 30000)).to.be.ok();
+        });
+
+        it("should trigger `reconnected` event", function() {
+          expect(this.hoodie.trigger.calledWith('reconnected')).to.be.ok();
+        });
+
+      });
+
+      _and("request fails", function() {
+
+        beforeEach(function() {
+          this.requestDefer.reject({
+            "status": 0,
+            "statusText": "Error"
+          });
+          this.hoodie.checkConnection();
+        });
+
+        it("should check again in 3 seconds", function() {
+          expect(window.setTimeout.calledWith(this.hoodie.checkConnection, 3000)).to.be.ok();
+        });
+
+        it("should not trigger `disconnected` event", function() {
+          expect(this.hoodie.trigger.calledWith('disconnected')).to.not.be.ok();
+        });
+
+      });
+
+    });
+
+  });
+
+  describe("#open(store, options)", function() {
+
+    it("should instantiate a Remote instance", function() {
+
+      this.sandbox.spy(Hoodie, 'Remote');
+
+      this.hoodie.open("store_name", {
+        option: "value"
+      });
+      expect(Hoodie.Remote.calledWith(this.hoodie, {
+        name: "store_name",
+        option: "value"
+      })).to.be.ok();
+    });
+
+  });
+
+  describe("#uuid(num = 7)", function() {
+
+    it("should default to a length of 7", function() {
+      expect(this.hoodie.uuid().length).to.eql(7);
+    });
+
+    _when("called with num = 5", function() {
+
+      it("should generate an id with length = 5", function() {
+        expect(this.hoodie.uuid(5).length).to.eql(5);
+      });
+
+    });
+
+  });
 
   //describe("#isPromise(object)", function() {
+
     //it("should return true if object is a promise", function() {
-      //var object;
-      //object = $.Deferred().promise();
+      //var object = $.Deferred().promise();
       //expect(this.hoodie.isPromise(object)).toBe(true);
     //});
+
     //it("should return false for deferred objects", function() {
-      //var object;
-      //object = $.Deferred();
+      //var object; = $.Deferred();
       //expect(this.hoodie.isPromise(object)).toBe(false);
     //});
+
     //it("should return false when object is undefined", function() {
       //expect(this.hoodie.isPromise(void 0)).toBe(false);
     //});
+
   //});
 
   //describe("#resolve()", function() {
@@ -323,15 +373,15 @@ describe("Hoodie", function() {
 
   //});
 
-  //describe("#dispose()", function() {
-    //beforeEach(function() {
-      //spyOn(this.hoodie, "trigger");
-    //});
+  describe("#dispose()", function() {
+    beforeEach(function() {
+      this.sandbox.spy(this.hoodie, 'trigger');
+    });
 
-    //it("should trigger `dispose` event", function() {
-      //this.hoodie.dispose();
-      //expect(this.hoodie.trigger).wasCalledWith('dispose');
-    //});
-  //});
+    it("should trigger `dispose` event", function() {
+      this.hoodie.dispose();
+      expect(this.hoodie.trigger.calledWith('dispose')).to.be.ok();
+    });
+  });
 
 });
